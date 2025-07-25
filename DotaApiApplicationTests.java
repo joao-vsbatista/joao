ea385b764f4b485a32e151271f9a24a47994a3d5
joao-vsbatista <joaovsb007@gmail.com>
2025-07-25 15:34:13 -0300

    Add files via upload

---
+++ DotaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dota_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
