1ce798eb39071a7aa0ee2f4eef879b241c300013
joao-vsbatista <joaovsb007@gmail.com>
2025-07-25 15:35:53 -0300

    Add files via upload

---
+++ DotaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dota_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaApiApplication.class, args);
+	}
+
+}
